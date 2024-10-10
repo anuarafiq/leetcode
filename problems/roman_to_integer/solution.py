d615702e1db12b1525aaeceef5d06eba63a94cff
Anuar Afiq <anuarafiq2407@gmail.com>
2024-10-10 12:22:03 +0000

    Sync LeetCode submission - Roman to Integer (python)

--- problems/roman_to_integer/solution.py
+++ problems/roman_to_integer/solution.py
@@ -1,9 +1,9 @@
 class Solution(object):
     def romanToInt(self, s):
         value = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-
         total = 0
         i = 0
+
         while i < len(s):
             if i < len(s) - 1 and value[s[i]] < value[s[i+1]]:
                 total += value[s[i+1]] - value[s[i]]
@@ -11,6 +11,4 @@ class Solution(object):
             else:
                 total += value[s[i]]
                 i += 1
-        
         return total
-        
