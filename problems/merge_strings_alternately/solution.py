5b56f6bf9ec58a43a5971ff5bff427e82bafb8db
Anuar Afiq <anuarafiq2407@gmail.com>
2024-10-12 07:02:12 +0000

    Sync LeetCode submission - Merge Strings Alternately (python)

--- problems/merge_strings_alternately/solution.py
+++ problems/merge_strings_alternately/solution.py
@@ -1,17 +1,19 @@
 class Solution(object):
     def mergeAlternately(self, word1, word2):
-        minimum = min(len(word1), len(word2))
-        maximum = max(len(word1), len(word2))
+        n = len(word1)
+        m = len(word2)
+        minimum = min(n, m)
+
         merged = ""
         for i in range(minimum):
             pointer1 = word1[i]
             pointer2 = word2[i]
             pair = pointer1 + pointer2
             merged += pair
-        
-        if len(word1) > len(word2):
-            merged += word1[len(word2):]
-        elif len(word1) < len(word2):
-            merged += word2[len(word1):]
+
+        if n < m:
+            merged += word2[n:]
+        else:
+            merged += word1[m:]  
 
         return merged
