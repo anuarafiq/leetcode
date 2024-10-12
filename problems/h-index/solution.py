58212881de258036370439947473d5257edd083a
Anuar Afiq <anuarafiq2407@gmail.com>
2024-10-12 13:40:11 +0000

    Sync LeetCode submission - H-Index (python)

---
+++ problems/h-index/solution.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def hIndex(self, citations):
+        n = len(citations)
+        paper_counts = [0] * (n+1)
+
+        for c in citations:
+            paper_counts[min(n, c)] += 1
+
+        h = n
+        papers = paper_counts[n]
+
+        while papers < h:
+            h -= 1
+            papers += paper_counts[h]
+
+        return h
