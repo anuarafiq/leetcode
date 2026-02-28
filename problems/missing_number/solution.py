ea3913ceff17feb769c0d308c8d378ec8ea87377
Anuar Afiq <anuarafiq2407@gmail.com>
2026-02-28 14:11:12 +0000

    Sync LeetCode submission - Missing Number (python3)

--- problems/missing_number/solution.py
+++ problems/missing_number/solution.py
@@ -1,11 +1,3 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        total = sum(nums)
-        maxx = max(nums)
-        nums_sum = (maxx * (maxx+1) // 2)
-
-        if total == nums_sum:
-            if nums.count(0) == 0:
-                return 0
-            return maxx + 1
-        return nums_sum - total
+        return (len(nums) * (len(nums) + 1) // 2) - sum(nums)
